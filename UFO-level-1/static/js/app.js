7c64b61ff33793add1b02b2e12e65193c3f27e12
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-01-23 22:00:08 -0800

    Cleaned up the trial codes and switched from function to arrow.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,56 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// tableData.forEach(info =>{
-//     Object.entries(info).forEach(([key, value]) =>{
-//         if (key === "datetime"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else if (key === "city"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else if (key === "state"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else if (key === "country"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else if (key === "shape"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else if (key === "durationMinutes"){
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }else{
-//             var row = d3.select("tbody").append("tr");
-//             row.append("td").text(value);
-//         }
-//     })
-// })
 
 // Use D3 to select the table body
 var tableBody = d3.select("tbody");
 
 // Loop through the data and append data to the table body
-// tableData.forEach(([datetime, city]) =>{
-//     var row = tableBody.append("tr");
-//     row.append("td").text(datetime);
-//     row.append("td").text(city);
-// });
-
-// ([datetime, city, state, country,
-//     shape, durationMinutes, comments])
-
-// tableData.forEach(([k, v]) =>{
-//     var row = tableBody.append("tr");
-//     row.append("td").text(datetime);
-//     row.append("td").text(city);
-// });
-
-data.forEach(function(tableData) {
+data.forEach(tableData => {
     console.log(tableData);
     var row = tableBody.append("tr");
-    Object.entries(tableData).forEach(function([key, value]) {
+    Object.entries(tableData).forEach(([key, value]) =>{
       console.log(key, value);
       row.append("td").text(value)
     });
