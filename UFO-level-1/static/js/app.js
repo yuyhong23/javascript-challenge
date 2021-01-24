309cab7cbfe7056ac80c03502ad42fccc7ca6b92
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-01-23 22:14:52 -0800

    Created codes that will listen to events and log the results.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,4 +14,36 @@ data.forEach(tableData => {
       console.log(key, value);
       row.append("td").text(value)
     });
-  });
+});
+
+//Use a date form in your HTML document and write JavaScript code that will listen for events 
+//and search through the date/time column to find rows that match user input
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  // Use the form input to filter the data by datetime
+  var filterData = tableData.filter(info => info.datetime === inputValue);
+  console.log(filterData);
+}
